b217c17c84b2adfd93a77704bf1c2b453c40834e
PC10 <PC10@PC10>
2016-01-11 10:43:23 -0500

    hola muando

---
+++ CalculadoraSI2/src/calculadorasi2/CalculadoraSI2.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadorasi2;
+
+/**
+ *
+ * @author Oswaldo Echeverria
+ */
+public class CalculadoraSI2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    
+    
+    public void MetdoSuma(){
+    
+    }
+    
+    public void MetodoResta(){
+    
+    }
+    
+    
+    public void MetdoMultiplicacion(){  
+    } 
+    
+    
+    
+}
